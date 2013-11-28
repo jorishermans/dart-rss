32cde03728c2bdce7c6f1e64d6dba27576faea47
joris hermans <JHermans85@hotmail.com>
2013-11-28 09:15:41 +0100

    and also a starting class

---
+++ lib/rss/rss_parser.dart
@@ -0,0 +1,14 @@
+part of dart_rss;
+
+class RssParser {
+  
+  XmlStreamer xmlStreamer;
+  XmlObjectBuilder xmlObjectBuilder;
+  
+  RssParser(String text) {
+    this.xmlStreamer = new XmlStreamer(text);
+    this.xmlObjectBuilder = new XmlObjectBuilder<RssItem>(xmlStreamer, new RssItemProcessor());
+  }
+  
+  Stream<RssItem> onProcess() => this.xmlObjectBuilder.onProcess();
+}
