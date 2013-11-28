6998a2aae83324fe2df34ae25b02dc53524d459f
joris hermans <JHermans85@hotmail.com>
2013-11-28 21:49:53 +0100

    updating part of for enclosure parsing

--- lib/dart_rss.dart
+++ lib/dart_rss.dart
@@ -8,5 +8,6 @@ part 'objects/rss_media_content.dart';
 part 'objects/rss_media_thumbnail.dart';
 part 'rss/rss_media_content_processor.dart';
 part 'rss/rss_media_thumbnail_processor.dart';
+part 'rss/rss_enclosure_processor.dart';
 part 'rss/rss_item_processor.dart';
 part 'rss/rss_parser.dart';
