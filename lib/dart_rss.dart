90688a09763c73ef2a04c01452f4a80396773b31
joris hermans <JHermans85@hotmail.com>
2013-11-28 21:25:15 +0100

    new rss item processor

--- lib/dart_rss.dart
+++ lib/dart_rss.dart
@@ -3,6 +3,10 @@ library dart_rss;
 import 'dart:async';
 import 'package:xmlstream/xmlstream.dart';
 
-part 'rss/rss_item.dart';
+part 'objects/rss_item.dart';
+part 'objects/rss_media_content.dart';
+part 'objects/rss_media_thumbnail.dart';
+part 'rss/rss_media_content_processor.dart';
+part 'rss/rss_media_thumbnail_processor.dart';
 part 'rss/rss_item_processor.dart';
 part 'rss/rss_parser.dart';
